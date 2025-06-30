1b878e65374b0be3d912399e8ab760af2ffcaf40
hyemin9999 <hmlim9999@gmail.com>
2025-06-30 12:32:22 +0900

    질문등록 기능 추가

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,9 +30,25 @@ public class QuestionController {
 
 	@GetMapping(value = "/detail/{id}")
 	public String detail(Model model, @PathVariable("id") Integer id) {
-		Question question = this.questionService.getQuestion(id);
-		model.addAttribute("question", question);
+
+		// TODO :: 서비스에서 가져옴.
+		Question q = this.questionService.getQuestion(id);
+		model.addAttribute("question", q);
 		return "question_detail";
+	}
+
+	@GetMapping("/create")
+	public String createQuestion() {
+		return "question_form";
+	}
+
+	@PostMapping("/create")
+	public String createQuestion(@RequestParam(value = "subject") String subject,
+			@RequestParam(value = "content") String content) {
+
+		// TODO :: 서비스에서 저장
+		this.questionService.create(subject, content);
+		return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
 	}
 
 }
--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,9 +1,12 @@
 package com.mysite.sbb.question;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+
+import com.mysite.sbb.DataNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,17 +24,22 @@ public class QuestionService {
 	public Question getQuestion(Integer id) {
 
 		// 가져온 값이 없을경우(=null)를 체크하기 위해. Optional 사용
-		Optional<Question> item = this.questionRepository.findById(id);
+		Optional<Question> q = this.questionRepository.findById(id);
 
-		if (item.isPresent()) {
-			return item.get();
+		if (q.isPresent()) {
+			return q.get();
+		} else {
+			throw new DataNotFoundException("question not found");
 		}
-		return null;
+	}
 
-//		if (item.isPresent()) {
-//			return item.get();
-//		} else {
-//			throw new DataNotFoundException("question not found");
-//		}
+	public void create(String subject, String content) {
+		Question q = new Question();
+
+		q.setSubject(subject);
+		q.setContent(content);
+		q.setCreateDate(LocalDateTime.now());
+
+		this.questionRepository.save(q);
 	}
 }
