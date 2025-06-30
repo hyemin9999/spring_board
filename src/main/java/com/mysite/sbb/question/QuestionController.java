6fc706752f7af830abd1004e252887c9dcdf7625
hyemin9999 <hmlim9999@gmail.com>
2025-06-30 17:21:14 +0900

    폼활용하기, 답변 등록 기능에 폼 적용, 공통 템플릿 만들기

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mysite.sbb.answer.AnswerForm;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/question")
@@ -29,7 +32,7 @@ public class QuestionController {
 	}
 
 	@GetMapping(value = "/detail/{id}")
-	public String detail(Model model, @PathVariable("id") Integer id) {
+	public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
 
 		// TODO :: 서비스에서 가져옴.
 		Question q = this.questionService.getQuestion(id);
@@ -38,16 +41,20 @@ public class QuestionController {
 	}
 
 	@GetMapping("/create")
-	public String createQuestion() {
+	public String createQuestion(QuestionForm questionForm) {
 		return "question_form";
 	}
 
+	// @RequestParam ==> @Valid QuestionForm
 	@PostMapping("/create")
-	public String createQuestion(@RequestParam(value = "subject") String subject,
-			@RequestParam(value = "content") String content) {
+	public String createQuestion(@Valid QuestionForm questionForm, BindingResult bindingResult) {
 
+		// TODO :: validation(유효성 검사 추가)
+		if (bindingResult.hasErrors()) {
+			return "question_form";
+		}
 		// TODO :: 서비스에서 저장
-		this.questionService.create(subject, content);
+		this.questionService.create(questionForm.getSubject(), questionForm.getContent());
 		return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
 	}
 
